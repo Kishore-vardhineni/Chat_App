f22163c134af5058f304d405f6bf8de79cd25869
kishor vardhineni <kishore.vardhineni01@gmail.com>
2025-07-28 11:24:48 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,12 @@
 const express = require('express');
-const cors = require('cors');
+const dotenv = require('dotenv');
+
 const app = express();
-require('dotenv').config();
-
-app.use(cors());
 app.use(express.json());
+dotenv.config();
 
-const PORT = process.env.PORT || 3000
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-    console.log(`Server is running on http://localhost:${PORT}`)
-})
+  console.log(`Server is running on port ${PORT}`);
+});
