0b452580e52681fdbf6d17be9a5a6061f6519525
kishor vardhineni <kishore.vardhineni01@gmail.com>
2025-07-28 12:15:34 +0530

    initial commit

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
 const app = express();
 app.use(express.json());
 dotenv.config();
 
 
+const connect = () => {
+   mongoose.connect(process.env.MONGO_URI)
+   .then(() => {
+      console.log("database connected");
+   }).catch(() => {
+      console.log("database not connected");
+   })
+}
+
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
+  connect();
   console.log(`Server is running on port ${PORT}`);
 });
